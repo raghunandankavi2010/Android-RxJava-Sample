bafc48c11dd172a1a5d0fe0259d02b9976fd192f
raghunandankavi2010 <raghunandankavi2010@gmail.com>
2015-10-07 22:58:43 +0530

    clean up

--- app/src/main/java/com/anupcowkur/mvpsample/ui/activities/NetworkModule.java
+++ app/src/main/java/com/anupcowkur/mvpsample/ui/activities/NetworkModule.java
@@ -1,23 +1,12 @@
 package com.anupcowkur.mvpsample.ui.activities;
 
 import com.anupcowkur.mvpsample.model.PostsAPI;
-import com.anupcowkur.mvpsample.model.data.Post;
 import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
-import java.io.IOException;
-import java.util.List;
-
 import dagger.Module;
 import dagger.Provides;
 import retrofit.GsonConverterFactory;
 import retrofit.RxJavaCallAdapterFactory;
-import retrofit.http.GET;
-import rx.Observable;
 import retrofit.Retrofit;
-
-import static java.lang.String.format;
 
 /**
  * Created by Raghunandan on 07-10-2015.
